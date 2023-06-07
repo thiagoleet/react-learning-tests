e7c24bf86e0e325eac948930c6c0c75faa098c9b
Thiago Leite <thiago.leite@agileengine.com>
2023-06-06 21:00:05 -0300

    Using a mock

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
+const DUMMY_DATA = [{ id: "p1", title: "first post" }];
+
 describe("Async.js", () => {
   test("renders posts if request succeeds", async () => {
+    // overwriting fetch method
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => DUMMY_DATA,
+    });
+
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole("listitem");
